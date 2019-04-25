f65f738083ab3a7c10c180f470c062d238281de0
0xflotus <0xflotus@gmail.com>
2019-04-25 19:30:16 +0200

    v0.0.8

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 module.exports = (g, _ = [...g]) =>
-  !/(^\d{8}$|^\d{12,14}$)/.test(g)
-    ? false
-    : _.pop() ==
-      10 -
-        (_.reverse()
-          .map((v, i) => v * (i % 2 == 0 ? 3 : 1))
-          .reduce((a, b) => a + b) %
-          10);
+  !!/(^\d{8}$|^\d{12,14}$)/.test(g) &&
+  _.pop() ==
+    10 -
+      (_.reverse()
+        .map((v, i) => v * (i % 2 == 0 ? 3 : 1))
+        .reduce((a, b) => a + b) %
+        10);
