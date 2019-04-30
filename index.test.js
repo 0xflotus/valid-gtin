36357d632fb9c3c40a6de03344f403dab7b3b239
0xflotus <0xflotus@gmail.com>
2019-04-30 18:39:50 +0200

    added support for Jest tests

---
+++ index.test.js
@@ -0,0 +1,11 @@
+const validate = require("./index");
+
+test("Should be valid GTINs", () => {
+  const VALIDS = ["97350053850012", "012345678905"];
+  VALIDS.forEach(gtin => expect(validate(gtin)).toBe(true));
+});
+
+test("Should be invalid GTINs", () => {
+  const INVALIDS = ["0024", "sffsgf", "0123345463"];
+  INVALIDS.forEach(gtin => expect(validate(gtin)).toBe(false));
+});
