71e00c5ebe5e409029d2ad8c1a2ab7da7b3e2057
0xflotus <0xflotus@gmail.com>
2020-12-09 23:55:26 +0100

    fixed code smell
    discovered by SonarCloud.io

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = (g, _ = [...g]) =>
   !!/(^\d{8}$|^\d{12,14}$)/.test(g) &&
-  _.pop() == 10 - ((_.reverse()
+  _.pop() == 10 - (([..._].reverse()
     .map((v, i) => v * (i % 2 == 0 ? 3 : 1))
     .reduce((a, b) => a + b) %
     10) || 0xA);
