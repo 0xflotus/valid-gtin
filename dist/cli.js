2393eb65edc7b0c40b1d2697c2adbc0c7a60cd7e
Rahul Aggarwal <91338578+Rahulagg13@users.noreply.github.com>
2024-10-07 14:09:09 +0530

    Remove compiled JS from repo and integrate build step before publishin (#92)
    * chore: remove compiled JS from repo and add prepublish build step
    
    * chore: remove compiled JS from repo and add prepublish build step
    
    ---------
    
    Co-authored-by: Rahul Aggarwal <rahulaggarwal@Rahuls-MacBook-Air.local>

--- dist/cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = __importDefault(require("./index"));
-const input = process.argv[2];
-console.log(!input ? "You didn't pass a GTIN" : (0, index_1.default)(input));
